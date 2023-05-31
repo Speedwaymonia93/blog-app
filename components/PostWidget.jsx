5b8fe9d632ada2daee19e3aeaff734538a055db6
Monika <monikastrzalka@gmail.com>
2023-05-31 12:56:46 +0200

    Recen posts glassmortphism effect.

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -20,7 +20,7 @@ const PostWidget = ({ categories, url }) => {
   let plTitle = url ? "Powiązane posty" : "Ostatnie posty";
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 bg-opacity-60 backdrop-filter backdrop-blur-lg">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
         {language === "en" ? enTitle : plTitle}
       </h3>
